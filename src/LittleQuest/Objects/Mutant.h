ecbd91d68aff9af3e9e66dbe01b99120c6f5f43f
ricky10099 <rickyto10099@gmail.com>
2023-12-11 14:07:27 +0900

    TODO: Add boss attack pattern

--- src/LittleQuest/Objects/Mutant.h
+++ src/LittleQuest/Objects/Mutant.h
@@ -9,7 +9,7 @@ USING_PTR(Mutant);
 class Mutant: public Enemy {
    public:
     BP_OBJECT_TYPE(Mutant, Enemy);
-    static EnemyPtr Create(const float3& pos, bool isPatrol = true, bool isBoss = false);
+    static MutantPtr Create(const float3& pos, bool isPatrol = true);
 
     bool Init() override;
     void Update() override;
